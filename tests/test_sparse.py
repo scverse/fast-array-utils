bd2bd9298eda4f7c72e54b283fc24a9e013e9bcc
Philipp A. <flying-sheep@web.de>
2025-01-27 13:18:49 +0100

    dtype

--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -12,11 +12,14 @@ from fast_array_utils.scipy.to_dense import to_dense
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from typing import Any, Literal
+    from typing import Literal, TypeVar
 
     from numpy.typing import NDArray
 
     from fast_array_utils.types import CSBase
+
+    DType = TypeVar("DType", bound=np.generic)
+    DType_float = TypeVar("DType_float", np.float32, np.float64)
 
 
 skip_if_no_scipy = pytest.mark.skipif(not find_spec("scipy"), reason="scipy not installed")
@@ -31,16 +34,22 @@ skip_if_no_scipy = pytest.mark.skipif(not find_spec("scipy"), reason="scipy not 
         pytest.param("csc_matrix", marks=skip_if_no_scipy),
     ],
 )
-def sp_cls(request: pytest.FixtureRequest) -> Callable[[NDArray[Any]], CSBase[Any]]:
+def sp_cls(request: pytest.FixtureRequest) -> Callable[[NDArray[DType]], CSBase[DType]]:
     from scipy import sparse
 
-    return getattr(sparse, request.param)
+    return getattr(sparse, request.param)  # type: ignore[no-any-return]
 
 
 @pytest.mark.parametrize("order", ["C", "F"])
-def test_to_dense(order: Literal["C", "F"], sp_cls: Callable[[NDArray[Any]], CSBase[Any]]) -> None:
+@pytest.mark.parametrize("dtype", [np.float32, np.float64])
+def test_to_dense(
+    order: Literal["C", "F"],
+    dtype: np.dtype[DType_float],
+    sp_cls: Callable[[NDArray[DType_float]], CSBase[DType_float]],
+) -> None:
     rng = np.random.default_rng()
-    mat = sp_cls(rng.random((10, 10)))
+    mat = sp_cls(rng.random((10, 10), dtype=dtype))
     arr = to_dense(mat, order=order)
     assert arr.flags[order]
+    assert arr.dtype == mat.dtype
     np.testing.assert_equal(arr, mat.toarray(order=order))
