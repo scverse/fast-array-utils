ec9af02c6df3dfcdd0ef4d4ac6099f109b497b1e
Philipp A. <flying-sheep@web.de>
2025-01-28 10:59:43 +0100

    improve sparse tests

--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: MPL-2.0
 from __future__ import annotations
 
-from importlib.util import find_spec
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -11,10 +10,7 @@ from fast_array_utils.scipy import to_dense
 
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
     from typing import Literal, TypeVar
-
-    from numpy.typing import NDArray
 
     from fast_array_utils.types import CSBase
 
@@ -22,34 +18,40 @@ if TYPE_CHECKING:
     DType_float = TypeVar("DType_float", np.float32, np.float64)
 
 
-skip_if_no_scipy = pytest.mark.skipif(not find_spec("scipy"), reason="scipy not installed")
+@pytest.fixture(scope="session", params=["csr", "csc"])
+def sp_fmt(request: pytest.FixtureRequest) -> Literal["csr", "csc"]:
+    return request.param  # type: ignore[no-any-return]
 
 
-@pytest.fixture(
-    scope="session",
-    params=[
-        pytest.param("csr_array", marks=skip_if_no_scipy),
-        pytest.param("csc_array", marks=skip_if_no_scipy),
-        pytest.param("csr_matrix", marks=skip_if_no_scipy),
-        pytest.param("csc_matrix", marks=skip_if_no_scipy),
-    ],
-)
-def sp_cls(request: pytest.FixtureRequest) -> Callable[[NDArray[DType]], CSBase[DType]]:
-    from scipy import sparse
+@pytest.fixture(scope="session", params=["matrix", "array"])
+def sp_container(request: pytest.FixtureRequest) -> Literal["matrix", "array"]:
+    return request.param  # type: ignore[no-any-return]
 
-    return getattr(sparse, request.param)  # type: ignore[no-any-return]
+
+@pytest.fixture(scope="session", params=[np.float32, np.float64])
+def dtype(request: pytest.FixtureRequest) -> np.dtype[np.float32 | np.float64]:
+    return np.dtype(request.param)
+
+
+@pytest.fixture
+def sp_mat(
+    sp_fmt: Literal["csr", "csc"],
+    sp_container: Literal["matrix", "array"],
+    dtype: np.dtype[DType_float],
+) -> CSBase[DType_float]:
+    pytest.importorskip("scipy")
+    from scipy.sparse import random, random_array
+
+    return (
+        random(10, 10, format=sp_fmt, dtype=dtype)
+        if sp_container == "matrix"
+        else random_array((10, 10), format=sp_fmt, dtype=dtype)
+    )
 
 
 @pytest.mark.parametrize("order", ["C", "F"])
-@pytest.mark.parametrize("dtype", [np.float32, np.float64])
-def test_to_dense(
-    order: Literal["C", "F"],
-    dtype: np.dtype[DType_float],
-    sp_cls: Callable[[NDArray[DType_float]], CSBase[DType_float]],
-) -> None:
-    rng = np.random.default_rng()
-    mat = sp_cls(rng.random((10, 10), dtype=dtype))
-    arr = to_dense(mat, order=order)
+def test_to_dense(order: Literal["C", "F"], sp_mat: CSBase[np.float64]) -> None:
+    arr = to_dense(sp_mat, order=order)
     assert arr.flags[order]
-    assert arr.dtype == mat.dtype
-    np.testing.assert_equal(arr, mat.toarray(order=order))
+    assert arr.dtype == sp_mat.dtype
+    np.testing.assert_equal(arr, sp_mat.toarray(order=order))
