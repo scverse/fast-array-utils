65b1529fc6f27136c5b421e557eee9ca7f089a28
Philipp A. <flying-sheep@web.de>
2025-02-14 16:01:06 +0100

    better docs

--- src/fast_array_utils/_patches.py
+++ src/fast_array_utils/_patches.py
@@ -12,15 +12,15 @@ def patch_dask() -> None:
     See <https://github.com/dask/dask/blob/4d71629d1f22ced0dd780919f22e70a642ec6753/dask/array/backends.py#L212-L232>
     """
     try:
-        from dask.array import dispatch
+        # Other lookup candidates: tensordot_lookup and take_lookup
+        from dask.array.dispatch import concatenate_lookup
         from scipy.sparse import sparray, spmatrix
     except ImportError:
+        return  # No need to patch if dask or scipy is not installed
+
+    # Avoid patch if already patched or upstream support has been added
+    if concatenate_lookup.dispatch(sparray) is not np.concatenate:  # type: ignore[no-untyped-call]
         return
 
-    if dispatch.concatenate_lookup.dispatch(sparray) is not np.concatenate:  # type: ignore[no-untyped-call]
-        return
-
-    concatenate = dispatch.concatenate_lookup.dispatch(spmatrix)  # type: ignore[no-untyped-call]
-    dispatch.concatenate_lookup.register(sparray, concatenate)  # type: ignore[no-untyped-call]
-
-    # Other lookup candidates: tensordot_lookup and take_lookup
+    concatenate = concatenate_lookup.dispatch(spmatrix)  # type: ignore[no-untyped-call]
+    concatenate_lookup.register(sparray, concatenate)  # type: ignore[no-untyped-call]
