8168c990eebc8feabb52f619675c0e1c97784e5d
Philipp A. <flying-sheep@web.de>
2025-02-20 13:12:29 +0100

    fix codspeed (#25)

--- tests/test_asarray.py
+++ tests/test_asarray.py
@@ -9,15 +9,11 @@ from fast_array_utils.conv import asarray
 
 
 if TYPE_CHECKING:
-    from typing import Any
-
-    from numpy.typing import NDArray
-
     from testing.fast_array_utils import ToArray
 
 
 def test_asarray(to_array: ToArray) -> None:
     x = to_array([[1, 2, 3], [4, 5, 6]])
-    arr: NDArray[Any] = asarray(x)  # type: ignore[arg-type]
+    arr = asarray(x)
     assert isinstance(arr, np.ndarray)
     assert arr.shape == (2, 3)
