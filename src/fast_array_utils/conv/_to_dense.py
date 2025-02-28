b0dcc90c9b17104ba490dbf63c3401e98f0bc93a
Philipp A. <flying-sheep@web.de>
2025-02-28 13:47:31 +0100

    Add to_memory param

--- src/fast_array_utils/conv/_to_dense.py
+++ src/fast_array_utils/conv/_to_dense.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from functools import singledispatch
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Any, cast, overload
 
 import numpy as np
 from numpy.typing import NDArray
@@ -11,61 +11,111 @@ from .. import types
 
 
 if TYPE_CHECKING:
-    from numpy.typing import ArrayLike
+    from typing import Literal, TypeAlias
+
+    Array: TypeAlias = (
+        NDArray[Any]
+        | types.CSBase
+        | types.DaskArray
+        | types.OutOfCoreDataset[Any]
+        | types.H5Dataset
+        | types.ZarrArray
+        | types.CupyArray
+        | types.CupySparseMatrix
+    )
 
 
 __all__ = ["to_dense"]
 
 
-# fallback’s arg0 type has to include types of registered functions
-@singledispatch
+@overload
+def to_dense(x: NDArray[Any] | types.CSBase, /, *, to_memory: bool = False) -> NDArray[Any]: ...
+
+
+@overload
+def to_dense(x: types.DaskArray, /, *, to_memory: Literal[False] = False) -> types.DaskArray: ...
+@overload
+def to_dense(x: types.DaskArray, /, *, to_memory: Literal[True]) -> NDArray[Any]: ...
+
+
+@overload
 def to_dense(
-    x: ArrayLike
-    | types.CSBase
-    | types.DaskArray
-    | types.OutOfCoreDataset[Any]
-    | types.H5Dataset
-    | types.ZarrArray
-    | types.CupyArray
-    | types.CupySparseMatrix,
-) -> NDArray[Any]:
-    """Convert x to a numpy array.
+    x: types.OutOfCoreDataset[types.CSBase | NDArray[Any]], /, *, to_memory: Literal[True]
+) -> NDArray[Any]: ...
+
+
+@overload
+def to_dense(  # type: ignore[overload-cannot-match]
+    x: types.CupyArray | types.CupySparseMatrix, /, *, to_memory: Literal[False] = False
+) -> types.CupyArray: ...
+@overload
+def to_dense(  # type: ignore[overload-cannot-match]
+    x: types.CupyArray | types.CupySparseMatrix, /, *, to_memory: Literal[True]
+) -> NDArray[Any]: ...
+
+
+def to_dense(
+    x: Array, /, *, to_memory: bool = False
+) -> NDArray[Any] | types.DaskArray | types.CupyArray:
+    """Convert x to a dense array.
 
     Parameters
     ----------
     x
         Input object to be converted.
+    to_memory
+        Also load data into memory (resulting in a :class:`numpy.ndarray`).
 
     Returns
     -------
-    Numpy array form of ``x``
+    Dense form of ``x``
 
     """
+    return _to_dense(x, to_memory=to_memory)
+
+
+# fallback’s arg0 type has to include types of registered functions
+@singledispatch
+def _to_dense(
+    x: Array, /, *, to_memory: bool = False
+) -> NDArray[Any] | types.DaskArray | types.CupyArray:
+    del to_memory  # it already is
     return np.asarray(x)
 
 
-@to_dense.register(types.CSBase)
-def _(x: types.CSBase) -> NDArray[Any]:
+@_to_dense.register(types.CSBase)
+def _(x: types.CSBase, /, *, to_memory: bool = False) -> NDArray[Any]:
     from . import scipy
 
+    del to_memory  # it already is
     return scipy.to_dense(x)
 
 
-@to_dense.register(types.DaskArray)
-def _(x: types.DaskArray) -> NDArray[Any]:
-    return to_dense(x.compute())  # type: ignore[no-untyped-call]
+@_to_dense.register(types.DaskArray)
+def _(x: types.DaskArray, /, *, to_memory: bool = False) -> NDArray[Any] | types.DaskArray:
+    if TYPE_CHECKING:
+        from dask.array.core import map_blocks
+    else:
+        from dask.array import map_blocks
+
+    x = cast(types.DaskArray, map_blocks(to_dense, x))  # type: ignore[no-untyped-call]
+    return x.compute() if to_memory else x  # type: ignore[no-untyped-call]
 
 
-@to_dense.register(types.OutOfCoreDataset)
-def _(x: types.OutOfCoreDataset[types.CSBase | NDArray[Any]]) -> NDArray[Any]:
-    return to_dense(x.to_memory())
+@_to_dense.register(types.OutOfCoreDataset)
+def _(
+    x: types.OutOfCoreDataset[types.CSBase | NDArray[Any]], /, *, to_memory: bool = False
+) -> NDArray[Any]:
+    if not to_memory:
+        msg = "to_memory must be True if x is an OutOfCoreDataset"
+        raise ValueError(msg)
+    # TODO(flying-sheep): why is to_memory of type Any?  # noqa: TD003
+    return to_dense(x.to_memory())  # type: ignore[no-any-return]
 
 
-@to_dense.register(types.CupyArray)
-def _(x: types.CupyArray) -> NDArray[Any]:
-    return cast(NDArray[Any], x.get())
-
-
-@to_dense.register(types.CupySparseMatrix)
-def _(x: types.CupySparseMatrix) -> NDArray[Any]:
-    return cast(NDArray[Any], x.toarray().get())
+@_to_dense.register(types.CupyArray | types.CupySparseMatrix)
+def _(
+    x: types.CupyArray | types.CupySparseMatrix, /, *, to_memory: bool = False
+) -> NDArray[Any] | types.CupyArray:
+    x = cast(types.CupyArray, x.toarray()) if isinstance(x, types.CupySparseMatrix) else x
+    return cast(NDArray[Any], x.get()) if to_memory else x
