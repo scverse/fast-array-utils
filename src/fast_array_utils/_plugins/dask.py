3813c069ee8f454bf8265be684d5c53defef7604
Philipp A. <flying-sheep@web.de>
2025-08-25 17:20:44 +0200

    fix: actually fix dask (#114)

--- src/fast_array_utils/_plugins/dask.py
+++ src/fast_array_utils/_plugins/dask.py
@@ -1,8 +1,14 @@
 # SPDX-License-Identifier: MPL-2.0
 from __future__ import annotations
 
-from dask.array.dispatch import concatenate_lookup, take_lookup, tensordot_lookup
+from dask.array.dispatch import concatenate_lookup, tensordot_lookup
 from scipy.sparse import sparray, spmatrix
+
+
+try:
+    from dask.array.dispatch import take_lookup
+except ImportError:
+    take_lookup = None
 
 
 # TODO(flying-sheep): upstream
@@ -13,9 +19,10 @@ def patch() -> None:  # pragma: no cover
     See <https://github.com/dask/dask/blob/d9b5c5b0256208f1befe94b26bfa8eaabcd0536d/dask/array/backends.py#L239-L241>
     """
     # Avoid patch if already patched or upstream support has been added
-    if concatenate_lookup.dispatch(sparray) is not concatenate_lookup.dispatch(spmatrix):
+    if concatenate_lookup.dispatch(sparray) is concatenate_lookup.dispatch(spmatrix):
         return
 
     concatenate_lookup.register(sparray, concatenate_lookup.dispatch(spmatrix))
     tensordot_lookup.register(sparray, tensordot_lookup.dispatch(spmatrix))
-    take_lookup.register(sparray, take_lookup.dispatch(spmatrix))
+    if take_lookup is not None:
+        take_lookup.register(sparray, take_lookup.dispatch(spmatrix))
